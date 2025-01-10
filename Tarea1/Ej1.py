5b8283bc77ca12fb1cd028658501b5d4d47631f1
chaima <amirou.septiembre@gmail.com>
2025-01-10 16:54:31 +0100

    1 commit , hasta el ejercicio4 de la tarea 1

---
+++ Tarea1/Ej1.py
@@ -0,0 +1,27 @@
+
+"""
+Ejercicio 1---------------------------------------------
+nombre= input("¿Cómo te llamas?: ")
+print("Hola", nombre)
+Ejercicio 2------------------------------------------------
+nombre= input("¿Cómo te llamas?: ")
+apellido= input("Y tu apellidos: ")
+print("Hola", nombre +" "+ apellido)
+Ejercicio 3------------------------------------------------
+print("¿Cómo llamas a un oso sin dientes?\n¡Un oso de goma! ")
+Ejercicio 4------------------------------------------------
+
+Ejercicio 5------------------------------------------------
+
+Ejercicio 6------------------------------------------------
+
+Ejercicio 7------------------------------------------------
+
+Ejercicio 8------------------------------------------------
+
+Ejercicio 9------------------------------------------------
+
+"""
+numero1 = int(input("Introduce un número: "))
+numero2 = int(input("Introduce otro número: "))
+print("El total es "+str(numero1+numero2))
