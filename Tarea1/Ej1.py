ca2661131225310fb861f8179a7e71203c190ffc
Chaima <1925046@alu.murciaeduca.es>
2025-01-11 20:56:12 +0100

    me quedan los ultimos 4 ejercicios

--- Tarea1/Ej1.py
+++ Tarea1/Ej1.py
@@ -10,18 +10,46 @@ print("Hola", nombre +" "+ apellido)
 Ejercicio 3------------------------------------------------
 print("¿Cómo llamas a un oso sin dientes?\n¡Un oso de goma! ")
 Ejercicio 4------------------------------------------------
-
+numero1 = int(input("Introduce un número: "))
+numero2 = int(input("Introduce otro número: "))
+print("El total es "+str(numero1+numero2))
 Ejercicio 5------------------------------------------------
-
+numero1 = int(input("Introduce el numero 1: " ))
+numero2 = int(input("Introduce el numero 2: " ))
+numero3 = int(input("Introduce el numero 3: " ))
+print("La respuesta es : "+ str((numero1+numero2)*numero3))
 Ejercicio 6------------------------------------------------
-
+tenia = int(input("Cuantas porciones de pizza tenias: "))
+comido = int(input("Cuantas porciones de pizza te has comido: " ))
+print("La respuesta es "+ str(tenia-comido))
 Ejercicio 7------------------------------------------------
-
+nombre = input("Como te llamas?: " )
+edad = int(input("Cuanto años tienes?: " ))
+print(nombre +" el próximo cumpleaños usted tendrá "+str(edad+1))
 Ejercicio 8------------------------------------------------
 
 Ejercicio 9------------------------------------------------
 
 """
-numero1 = int(input("Introduce un número: "))
-numero2 = int(input("Introduce otro número: "))
-print("El total es "+str(numero1+numero2))
+
+total = int(input("Precio total de la cuenta: "))
+comensales = int(input("Numero de comensales: "))
+print("Precio a pagar cada comensal: "+ str(total/comensales))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
