ce505de822310d3df48e168f9bac31b08b390bc3
Chaima <1925046@alu.murciaeduca.es>
2025-02-18 01:21:14 +0100

    mitad de fuinciones

---
+++ Tarea4Fuciones/Funciones.py
@@ -0,0 +1,50 @@
+"""
+Ejercicio 118---------------------------------------------
+def suma(a, b):
+    return a + b
+
+print(suma(3, 10))
+Ejercicio 119---------------------------------------------
+def es_par(n):
+    return n % 2 == 0
+
+print(es_par(8))
+Ejercicio 120---------------------------------------------
+def factorial(n):
+    resultado = 1
+    for i in range(1, n + 1):
+        resultado *= i
+    return resultado
+
+print(factorial(6))
+Ejercicio 121---------------------------------------------
+def area_rectangulo(base, altura):
+    return base * altura
+
+print(area_rectangulo(4, 6))
+Ejercicio 122---------------------------------------------
+def mayusculas(cadena):
+    return cadena.upper()
+
+print(mayusculas("pepe"))
+Ejercicio 123---------------------------------------------
+def concatenar(cadena1, cadena2):
+    return cadena1 + cadena2
+print(concatenar("Soy ", "pepe!"))
+
+Ejercicio 124---------------------------------------------
+def longitud(cadena):
+    contador = 0
+    for _ in cadena:
+        contador += 1
+    return contador
+
+print(longitud("pepe happy"))
+------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------
+Lectura y Escritura de archivos en Python
+------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------
+Ejercicio 125---------------------------------------------
+
+"""
